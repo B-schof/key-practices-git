60d311a7e0e57db84882642df5c66a38eb62781d
Schofield <beatrice.schofield@let.ru.nl>
2021-04-13 09:57:43 +0200

    Cleaned up code per recommendations from KPLS seminar: Section headers added, variable names standardised, comments cleaned up

--- session4.R
+++ session4.R
@@ -1,19 +1,47 @@
-# 1. Set up your project by loading your packages, e.g.:
+# Date: 09.04.2021
+# Author: B.E. Schofield
+# File name: 'session4.R'
+# Description: Example script used to practice using Git and best coding practices
+# Project: Key Practices for the Language Scientist 2021
+
+
+# ---- Change log ----
+
+# Date: 09.04 
+
+# Change by: B.E. Schofield
+
+# Change: changed the layout and added header to script, added notes to script, created sections of code
+
+# Purpose: Clean up script as per homework instructions
+
+
+# ---- load packages ----
+
 library(package)
+library(here)
 
-# 2. Set the working directory and load the data, e.g.:
+# ---- data parameters ----
+
+# check workspace to ensure file in project path
+file_path <- here::here()
+
+# set working directory (if needed) and load data
+
 setwd("directory")
-data <- read.delim("data.csv", header = TRUE, sep = ",")
+example_data <- read.delim("data.csv", header = TRUE, sep = ",")
 
-# 3. Make variables ready to work on, e.g.:
-data <- data %>%
+# ---- data analysis ----
+
+# Make variables ready to work on, e.g.:
+example_data <- example_data %>%
   select(ppID, condition, trial, RT) %>%
   mutate(condition = as.factor(condition), target = as.factor(target))
-data <- na.omit(data)
+example_data <- na.omit(example_data)
 
-# 4. Explore data, e.g.:
-summary(data)
+# Explore data
+summary(example_data)
 
-# 5. Analysis (can be a simple analysis), e.g. a one-way ANOVA, within-subjects:
-anovaRTbycategory <- aov(RT ~category+error(ppID/category), data = analysis_anova)
-summary(anovaRTbycategory)
+# Within-subjects one-way ANOVA Analysis (can be a simple analysis), e.g. a one-way ANOVA, within-subjects:
+anovaRT_by_category <- aov(RT ~category+error(ppID/category), data = analysis_anova)
+summary(anovaRT_by_category)
